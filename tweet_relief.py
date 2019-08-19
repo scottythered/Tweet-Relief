45e18347cbe75b926461f638c0dbbf0a10b989eb
Scotty Carlson <scottythered@users.noreply.github.com>
2019-08-19 13:07:17 -0700

    Update tweet_relief.py

--- tweet_relief.py
+++ tweet_relief.py
@@ -7,7 +7,6 @@ from time import sleep
 from gooey import Gooey, GooeyParser
 from datetime import datetime
 import os
-import pytz
 
 
 @Gooey(program_name='Tweet Relief', show_sidebar=False, disable_stop_button=True)
@@ -16,17 +15,20 @@ def main():
     desc = 'A simplified GUI for wiping your Twitter history'
     parser = GooeyParser(description=desc)
     
-    creds_parser = parser.add_argument_group('First, enter your Twitter API credentials:', gooey_options={'show_border': False, 'columns': 2})
+    creds_parser = parser.add_argument_group('First, enter your Twitter API credentials:',
+                                             gooey_options={'show_border': False, 'columns': 2})
     creds_parser.add_argument('consumer_key', metavar='Consumer Key', widget='PasswordField')
     creds_parser.add_argument('consumer_secret', metavar='Consumer Secret:', widget='PasswordField')
     creds_parser.add_argument('access_token', metavar='Access Token:', widget='PasswordField')
     creds_parser.add_argument('access_token_secret', metavar='Access Token Secret:', widget='PasswordField')
     
     date_parser = parser.add_argument_group('Select your cutoff date')
-    date_parser.add_argument('stop_date', metavar='Tweets published before this date will be deleted:', widget='DateChooser')
+    date_parser.add_argument('stop_date', metavar='Tweets published before this date will be deleted:',
+                             widget='DateChooser')
 
     keepers = parser.add_argument_group('Keeping any tweets?')
-    keepers.add_argument('keepers', default='None', metavar='Enter tweet IDs you want to keep, separated by commas. (If not, leave as \'None\'.)')
+    keepers.add_argument('keepers', default='None', metavar='Enter tweet IDs you want to keep, separated by commas. '
+                         '(If not, leave as \'None\'.)')
 
     args = parser.parse_args()
     
@@ -49,10 +51,11 @@ def main():
     api = tweepy.API(auth)
     
     tweets = get_tweets(api) 
-    if keeping_tweets:
+    
+    try:
         nonkeepers = [tweet for tweet in tweets if tweet.id not in keeping_tweets]
         destroyer(nonkeepers, api, args.stop_date) 
-    else:
+    except NameError:
         destroyer(tweets, api, args.stop_date)
 
 
@@ -89,23 +92,21 @@ def get_tweets(api_object):
         all_tweets.extend(new_tweets)
         oldest = all_tweets[-1].id - 1
         print('{0} tweets grabbed so far...'.format(len(all_tweets)))
-        sleep(2)
+        sleep(1.5)
     print('Grabbed {0} tweets...'.format(len(all_tweets)))
     return all_tweets
 
 
 def destroyer(tweet_objects, api_object, arg_2):
     print('Destroying tweets... This might take a while.')
-    utc = pytz.utc
-    cutoff_date = utc.localize(datetime.strptime(arg_2, '%Y-%m-%d'))
+    cutoff_date = datetime.strptime(arg_2, '%Y-%m-%d')
     destroyed = []
     for tweet in tweet_objects:
-        create_date = datetime.strptime(tweet.created_at, "%a %b %d %H:%M:%S %z %Y")
-        if create_date < cutoff_date:
+        if tweet.created_at < cutoff_date:
             api_object.destroy_status(tweet.id)
             print('Tweet destroyed: {0}'.format(tweet.id))
             destroyed.append(tweet.id)
-            sleep(0.1)
+            sleep(0.2)
         else:
             pass
     print('Destruction complete, scrubbed {0} tweets from your timeline.'.format(len(destroyed)))
