44641993a39912a2eaa7d9a66829c01878f35531
Scotty Carlson <scottythered@users.noreply.github.com>
2019-08-19 11:11:51 -0700

    Update tweet_relief.py

--- tweet_relief.py
+++ tweet_relief.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import tweepy
 import json
 from time import sleep
@@ -27,31 +30,34 @@ def main():
 
     args = parser.parse_args()
     
+    consumer_key = args.consumer_key
+    consumer_secret = args.consumer_secret
+    access_token = args.access_token
+    access_token_secret = args.access_token_secret
+    
     if args.keepers == 'None':
         if os.path.exists(os.path.join('data', 'keepers.json')):
             with open(os.path.join('data', 'keepers.json'), 'r') as j:
-                keepers = json.load(j)
+                keeping_tweets = json.load(j)
         else:
             pass
     else:
-        keepers = trapper_keeper()
+        keeping_tweets = trapper_keeper(args.keepers)
     
-    api = authorize()
-    tweets = get_all_tweets() 
-    if keepers:
-        nonkeepers = [tweet for tweet in tweets if tweet.id not in keepers]
-        destroyer(nonkeepers) 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    tweets = get_tweets(api) 
+    if keeping_tweets:
+        nonkeepers = [tweet for tweet in tweets if tweet.id not in keeping_tweets]
+        destroyer(nonkeepers, api, args.stop_date) 
     else:
-        destroyer(tweets)
-  
-  
-def authorize():
-    auth = tweepy.OAuthHandler(args.consumer_key, args.consumer_secret)
-    auth.set_access_token(args.access_token, args.access_token_secret)
-    return tweepy.API(auth)
+        destroyer(tweets, api, args.stop_date)
 
-def trapper_keeper():
-    splits = ','.split(args.keepers)
+
+def trapper_keeper(arg_1):
+    splits = ','.split(arg_1)
     splits = [int(tweet.strip()) for tweet in splits]
     if os.path.exists(os.path.join('data', 'keepers.json')):
         with open(os.path.join('data', 'keepers.json'), 'r') as j:
@@ -72,30 +78,31 @@ def trapper_keeper():
             return splits
 
 
-def get_all_tweets():
-    screen_name = (api.me()).screen_name
+def get_tweets(api_object):
+    screen_name = (api_object.me()).screen_name
     all_tweets = []
-    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+    new_tweets = api_object.user_timeline(screen_name=screen_name, count=200)
     all_tweets.extend(new_tweets)
-    oldest = alltweets[-1].id - 1
+    oldest = all_tweets[-1].id - 1
     while len(new_tweets) > 0:
-        new_tweets = api.user_timeline(screen_name=xx, count=200, max_id=oldest)
+        new_tweets = api_object.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
         all_tweets.extend(new_tweets)
-        oldest = alltweets[-1].id - 1
+        oldest = all_tweets[-1].id - 1
         print('{0} tweets grabbed so far...'.format(len(all_tweets)))
         sleep(2)
-    print('Grabbed {0} tweets...'.format(len(all_tweets))
+    print('Grabbed {0} tweets...'.format(len(all_tweets)))
     return all_tweets
 
 
-def destroyer(tweet_objects):
+def destroyer(tweet_objects, api_object, arg_2):
     print('Destroying tweets... This might take a while.')
-    cutoff_date = utc.localize(datetime.strptime(args.stop_date, '%Y-%m-%d'))
+    utc = pytz.utc
+    cutoff_date = utc.localize(datetime.strptime(arg_2, '%Y-%m-%d'))
     destroyed = []
     for tweet in tweet_objects:
         create_date = datetime.strptime(tweet.created_at, "%a %b %d %H:%M:%S %z %Y")
         if create_date < cutoff_date:
-            api.destroy_status(tweet.id)
+            api_object.destroy_status(tweet.id)
             print('Tweet destroyed: {0}'.format(tweet.id))
             destroyed.append(tweet.id)
             sleep(0.1)
